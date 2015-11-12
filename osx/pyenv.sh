3078b62b2d38049973e59853b4b72e6c0b07d00e
Nick Frost <nickfrostatx@gmail.com>
2015-11-12 12:41:33 -0800

    Add pyenv install script

---
+++ osx/pyenv.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export PIP_REQUIRE_VIRTUALENV=''
+for vers in 3.5.0 3.4.3 3.3.6 3.2.6 2.7.10 2.6.9 pypy-4.0.0 pypy3-2.4.0; do
+    pyenv install $vers
+    PYENV_VERSION=$vers pip install --upgrade pip setuptools
+done
+pyenv rehash
+pyenv global 2.7.10 3.5.0
+
+PYENV_VERSION=3.5.0 pip install --upgrade httpie tox
