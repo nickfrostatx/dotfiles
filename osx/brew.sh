a8379df182ff8fd36397fe4d56836344cebb9fe6
Nick Frost <nickfrostatx@gmail.com>
2015-11-18 13:19:41 -0800

    Install docker

--- osx/brew.sh
+++ osx/brew.sh
@@ -6,7 +6,7 @@ brew upgrade --all
 brew cask update
 
 # Install homebrew packages
-brew install gcc git gnupg node optipng pyenv pyenv-virtualenv vim zopfli zsh
+brew install boot2docker docker gcc git gnupg node optipng pyenv pyenv-virtualenv vim zopfli zsh
 
 # Install casks
 brew cask install google-chrome kodi limechat sublime-text transmission vlc
