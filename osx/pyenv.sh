45e32093c9926543ef14386ceee7902b3451035b
Nick Frost <nickfrostatx@gmail.com>
2015-11-22 12:05:43 -0800

    Give the path for pyenv pip during installation

--- osx/pyenv.sh
+++ osx/pyenv.sh
@@ -2,9 +2,9 @@
 export PIP_REQUIRE_VIRTUALENV=''
 for vers in 3.5.0 3.4.3 3.3.6 3.2.6 2.7.10 2.6.9 pypy-4.0.0 pypy3-2.4.0; do
     pyenv install $vers
-    PYENV_VERSION=$vers pip install --upgrade pip setuptools
+    PYENV_VERSION=$vers $HOME/.pyenv/shims/pip install --upgrade pip setuptools
 done
 pyenv rehash
 pyenv global 2.7.10 3.5.0
 
-PYENV_VERSION=3.5.0 pip install --upgrade httpie tox
+PYENV_VERSION=3.5.0 $HOME/.pyenv/shims/pip install --upgrade httpie tox
