3ac58b06040a3cc85bd7b34ef4c0d8de8d8cf646
Nick Frost <nickfrostatx@gmail.com>
2015-12-27 21:21:36 -0600

    Fix home directory path issue

--- copy.sh
+++ copy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-SOURCE_DIR="~/.dotfiles"
+SOURCE_DIR="$HOME/.dotfiles"
 
 for f in .aliases .gitconfig .gitignore_global .profile .vimrc .zshrc; do
-    [[ "$SOURCE_DIR/$f" -nt "~/$f" ]] && cp -v "$SOURCE_DIR/$f" "~/$f"
+  [[ "$SOURCE_DIR/$f" -nt "$HOME/$f" ]] && cp -v "$SOURCE_DIR/$f" "$HOME/$f"
 done
 
 
