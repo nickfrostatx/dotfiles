668f099cf94b8ac1cf6d5ef395e39d797d67cef6
Nick Frost <nickfrostatx@gmail.com>
2016-04-12 22:06:25 -0700

    I don't use sublime

--- copy.sh
+++ copy.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 SOURCE_DIR="$HOME/.dotfiles"
-SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 2/Packages/User"
 
 for f in .aliases .gitconfig .gitignore_global .profile .vimrc .zshrc; do
   [[ "$SOURCE_DIR/$f" -nt "$HOME/$f" ]] && cp -v "$SOURCE_DIR/$f" "$HOME/$f"
 done
-
-if [[ -d "$SUBLIME_DIR" ]]; then
-  for f in "$SOURCE_DIR/sublime/"*.sublime-settings; do
-    DEST="$SUBLIME_DIR/$(basename $f)"
-    [[ "$f" -nt "$DEST" ]] && cp -v "$f" "$DEST"
-  done
-fi
