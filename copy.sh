f8e8646825226cbe3fee4c276bcb100b7662f0e9
Nick Frost <nickfrostatx@gmail.com>
2016-07-25 19:24:24 -0700

    Remove stuff I don't use

--- copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-SOURCE_DIR="$HOME/.dotfiles"
-
-for f in .aliases .gitconfig .gitignore_global .profile .vimrc .zshrc; do
-  [[ "$SOURCE_DIR/$f" -nt "$HOME/$f" ]] && cp -v "$SOURCE_DIR/$f" "$HOME/$f"
-done
